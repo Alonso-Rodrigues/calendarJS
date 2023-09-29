356cf8d9b0c514699a0482903d2ce789f35eb258
Alonso <alonso.rbass@gmail.com>
2023-09-29 13:58:00 +0200

    merge calender

--- assets/js/script_calender.js
+++ assets/js/script_calender.js
@@ -52,8 +52,3 @@ prevNextIcon.forEach(icon => {
     })
 });const currentData = document.querySelector(".current-date");
 
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
-
-console.log(date, currYear, currMonth);
