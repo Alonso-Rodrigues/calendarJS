0a297ea502b4fadc00cfa17b61aaaf259789b44f
Alonso <alonso.rbass@gmail.com>
2023-09-29 08:57:51 +0200

    feat: "console.log of current date"

---
+++ assets/js/script_calender.js
@@ -0,0 +1,7 @@
+const currentData = document.querySelector(".current-date");
+
+let date = new Date(),
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
+
+console.log(date, currYear, currMonth);
